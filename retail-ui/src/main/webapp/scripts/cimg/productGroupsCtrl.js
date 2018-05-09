91ee53fa755f5961c63e2955528722220e023e02
Arash Roudi <arash.khoshraftarroudi@transport.nsw.gov.au>
2018-05-09 18:00:23 +1000

    reporting completed

--- retail-ui/src/main/webapp/scripts/cimg/productGroupsCtrl.js
+++ retail-ui/src/main/webapp/scripts/cimg/productGroupsCtrl.js
@@ -1,84 +1,28 @@
 /**
  * Created by arash on 14/08/2015.
  */
-cimgApp.controller('reportingCtrl', function($scope, baseDataService, ngDialog, SUCCESS, FAILURE, REPORTING_GET_REPORT_LIST_URI) {
+cimgApp.controller('productGroupsCtrl', function($scope, baseDataService, SUCCESS, FAILURE, PRGP_GET_ALL_TREEVIEW_URI) {
+
 
     $scope.productGroupTreeOptions = {
         multipleSelect: true
     };
 
-    $scope.$on('selection-changed', function(e, node) {
-        $scope.selectedNode = node;
-        enableFilters();
+    $scope.$on('selection-changed', function(e, nodes) {
+        $scope.selectedNodes = nodes;
     });
 
-    function initReportFilters() {
-        $scope.dateFilterEnabled = false;
-        $scope.rangeFilterEnabled = false;
-        $scope.categoryFilterEnabled = false;
-        $scope.supplierFilterEnabled = false;
-        $scope.customerFilterEnabled = false;
-        $scope.staffFilterEnabled = false;
-        $scope.groupByFilterEnabled = false;
-        $scope.sortByFilterEnabled = false;
-    }
-
-    loadReportingList();
-    function loadReportingList() {
-        baseDataService.getBaseData(REPORTING_GET_REPORT_LIST_URI).then(function(response){
-            $scope.reportTree = response.data;
+    initPage();
+    function initPage() {
+        baseDataService.getBaseData(PRGP_GET_ALL_TREEVIEW_URI).then(function(response){
+            $scope.productGroupTree = response.data;
         });
-        initReportFilters();
     }
-
-    function trimTreeViewNodeObject(treeViewNode) {
-        delete treeViewNode.parentId;
-        delete treeViewNode.nodeId;
-        delete treeViewNode.selected;
-        delete treeViewNode.expanded;
-        delete treeViewNode.children;
+    $scope.submit = function () {
+        $scope.confirm($scope.selectedNodes);
     }
-
-    function enableFilters() {
-        initReportFilters();
-
-        if ($scope.selectedNode === undefined || $scope.selectedNode.nodeType==='REPORT_GROUP' || $scope.selectedNode.reportParamList===undefined) {
-            return;
-        }
-
-        if ($scope.selectedNode.reportParamList.length < 1) {
-            return;
-        }
-
-        for (var i=0; i < $scope.selectedNode.reportParamList.length ; i++ ) {
-
-            switch ($scope.selectedNode.reportParamList[i].repParamName) {
-                case 'Date' :
-                    $scope.dateFilterEnabled = true;
-                    break;
-                case 'Range' :
-                    $scope.rangeFilterEnabled = true;
-                    break;
-                case 'Category' :
-                    $scope.categoryFilterEnabled = true;
-                    break;
-                case 'Supplier' :
-                    $scope.supplierFilterEnabled = true;
-                    break;
-                case 'Customer' :
-                    $scope.customerFilterEnabled = true;
-                    break;
-                case 'Staff' :
-                    $scope.staffFilterEnabled = true;
-                    break;
-                case 'GroupBy' :
-                    $scope.groupByFilterEnabled = true;
-                    break;
-                case 'SortBy' :
-                    $scope.sortByFilterEnabled = true;
-                    break;
-            }
-        }
+    $scope.cancel = function() {
+        $scope.closeThisDialog('button');
     }
 
 });
