05b0220a770e9f352db43114d1410337238423b9
akhoshraft <arash.roudi@transport.nsw.gov.au>
2021-08-26 16:36:31 +1000

    Initial configuration for spring boot

---
+++ retail-web-services/src/main/java/au/com/biztune/retail/Application.java
@@ -0,0 +1,19 @@
+package au.gov.nsw.railcorp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot Application class.
+ */
+
+@SpringBootApplication
+public class Application {
+    /**
+     * Main method of Spring Boot Application class.
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
